9b9b4806ca50df98c78ee6b938d442b743ee9b74
Max Bagno <mbv1610@gmail.com>
2022-03-08 20:22:36 -0300

    exercicio 6 Array e loop For

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-02/exercicio6.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let impares = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] % 2 === 1) {
+        impares += 1;
+    }
+}
+    if (impares > 0) {
+ console.log(impares)
+    } else {
+        console.log('nenhum valor ímpar encontrado')
+    }
